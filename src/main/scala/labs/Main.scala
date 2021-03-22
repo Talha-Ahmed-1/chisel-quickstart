db0f901ecec9d85f45a96bdecb8d3b00b57a8140
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-03-22 12:41:39 +0500

    Lab 2 Task 2

--- src/main/scala/labs/Main.scala
+++ src/main/scala/labs/Main.scala
@@ -5,19 +5,20 @@ import chisel3.util._
 
 object Main extends App{
     println("-----------------------")
-    class Counter ( counterBits : UInt ) extends Module {
-        val io = IO ( new Bundle {
-          val result = Output ( Bool () )
-        })
+    // class Counter ( counterBits : UInt ) extends Module {
+    //     val io = IO ( new Bundle {
+    //       val result = Output ( Bool () )
+    //     })
 
-        val max = (1. U << counterBits ) - 1. U
-        val count = RegInit (0. U (16. W ) )
-        when ( count === max ) {
-            count := 0. U
-        } .otherwise {
-            count := count + 1. U
-        }
-        io . result := count (15. U )
-        println ( s" counter created with max value $max " )
-        }
+    //     val max = (1. U << counterBits ) - 1. U
+    //     val count = RegInit (0. U (16. W ) )
+    //     when ( count === max ) {
+    //         count := 0. U
+    //     } .otherwise {
+    //         count := count + 1. U
+    //     }
+    //     io . result := count (15. U )
+    //     println ( s" counter created with max value $max " )
+    //     }
+
 }
